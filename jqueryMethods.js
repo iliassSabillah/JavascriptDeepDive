0517b1d03ef8f4d32b8e6568ae4ba0ca12d29f44
iliass <iliass24@gmail.com>
2017-04-30 00:59:28 -0400

    implement isArrayLike method

--- jqueryMethods.js
+++ jqueryMethods.js
@@ -26,5 +26,23 @@ $.isArray=  obj =>{
     return Object.prototype.toString.call(obj) === "[object Array]"
 };
 
-console.log($.isArray([1,2,3]));
+// console.log($.isArray([1,2,3]));
 
+//implement the $.isArrayLike method
+
+$.isArrayLike = obj => {
+
+    if(typeof obj.length==='number'){
+        if(obj.length === 0 ){
+            return true;
+        }else if(obj.length > 0){
+            //checks if the last property is in the array
+            return (obj.length-1) in obj
+        }
+    }
+
+    return false;
+
+};
+
+console.log('is Array like',$.isArrayLike({0:"zero",1:"one" ,2:"two",length: 3}));
